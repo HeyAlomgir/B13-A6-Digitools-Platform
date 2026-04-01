57853aa7f0586e60264f3efad103e905ae1a6d5d
HeyAlomgir <alomgirhosssain71@gmail.com>
2026-04-01 18:14:36 +0600

    cart complte

--- src/Component/Step.jsx
+++ src/Component/Step.jsx
@@ -10,13 +10,16 @@ const Step = ({stepPromise}) => {
             <p className=' text-[#777777]'>Start using premium digital tools in minutes, not hours.</p>
             </div>
 
-            <div>
-                {stepData.map(steps => <div>
-                    <img src={steps.icon} alt="" />
-                    <h3>{steps.title} </h3>
-                    <p>{steps.description} </p>
-                    <div>
-                        <p>{steps.id} </p>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 '>
+                {stepData.map(steps => <div className='shadow-2xl  p-5  relative m-4 rounded-xl transform transition hover:scale-105 text-center space-y-5'>
+
+                    
+                        <img className=' mx-auto mt-10 bg-blue-100 rounded-full p-4  ' src={steps.icon} alt="" />
+          
+                    <h3 className='text-2xl'>{steps.title} </h3>
+                    <p className='text-[#777777]'>{steps.description} </p>
+                    <div className=' absolute top-3 right-5'>
+                        <p className='bg-[#4F39F6] px-1 py-1 text-white rounded-full'>{steps.id} </p>
                     </div>
                 </div> )}
             </div>
